21fb5751098abdcc425e1cf6fd3d12409579b8bc
KibiBC <69991794+Lv2Kode2Pwr@users.noreply.github.com>
2025-09-23 12:20:28 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: '/pathfinder-ai-64/',
   server: {
     host: "::",
     port: 8080,
